d122c395c7fc6286f3b7e967906efc9b5f0e6c6b
Yernar Kabdyzhanov <yernar.kabdyzhanov@gmail.com>
2026-06-24 23:39:46 +0500

    Initial commit: KZT currency calculator PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'kzt-calc-v1';
+const ASSETS = [
+  './currency-calculator.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => !a.endsWith('.png'))))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+
+  if (url.includes('cdn.jsdelivr.net')) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
